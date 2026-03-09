c29c081e4679680bbea458cdf2c63e24ec764b6b
shadstoneofficial <100179066+shadstoneofficial@users.noreply.github.com>
2026-03-09 15:17:10 +0700

    manifest and index ts

--- index.ts
+++ index.ts
@@ -2,6 +2,14 @@
 import { powerLobsterChannel } from './src/channel';
 export { getTools } from './src/tools';
 
-export default function(api: any) {
-  api.registerChannel({ plugin: powerLobsterChannel });
-}
+const plugin = {
+  id: "powerlobster",
+  name: "PowerLobster",
+  description: "PowerLobster channel plugin",
+  configSchema: { type: "object", additionalProperties: false, properties: {} },
+  register(api: any) {
+    api.registerChannel({ plugin: powerLobsterChannel });
+  },
+};
+
+export default plugin;
